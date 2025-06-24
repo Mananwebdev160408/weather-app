54d7649b45c1d8f07161cea70842812fb6ed83bd
Manan Gupta <guptamanan576@gmail.com>
2025-06-24 12:02:43 +0530

    completed web app

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let year=today.getFullYear()
 let day_num=today.getDay()
 let city_name=document.getElementsByTagName('p')[0]
 let country_name=document.getElementsByTagName('p')[1]
+let temp_c_value=document.getElementsByTagName('p')[4]
+let humidity1_value=document.getElementsByTagName('h1')[0]
+let wind1_value=document.getElementsByTagName('h1')[1]
+let gust1_value=document.getElementsByTagName('h1')[2]
 async function weather(city,country) {
     
 
@@ -21,10 +25,22 @@ try {
 	const response = await fetch(url, options);
 	const result = await response.json();
 	console.log(result)
-	
+	var city_name_value=result.location.name
+	var country_name_value=result.location.country
+	city_name.innerHTML=city_name_value
+	country_name.innerHTML=country_name_value
+	var temp_value=result.current.temp_c
+	temp_c_value.innerHTML=temp_value
+	var humidity_value=result.current.humidity
+	var wind_value=result.current.wind_kph
+	var gust_value=result.current.gust_kph
+	humidity1_value.innerHTML=humidity_value
+	wind1_value.innerHTML=wind_value
+	gust1_value.innerHTML=gust_value
+
     
 } catch (error) {
-	console.error(error);
+	alert('Something went wrong');
 }
 
 
